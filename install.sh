32d36e511a74be5ede6f696c88cbe1811ce51232
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-01-23 14:32:53 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,27 +1,8 @@
 #Clover Rescue Team's official Cloverside software installer
 
-
-echo "All is ready to install CloverRescue. Required free disk space - 12.5 MB. Continue? y/n: "
-read install
-if [ $install == "y" ]
-then
-    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.tgz -P ./ && tar xvzf ./ngrok-stable-linux-arm.tgz -C ./
-    rm ./ngrok-stable-linux-arm.tgz
-    git clone https://github.com/DevMBS/CRTClover.git
-    echo "Please enter your data correctly, otherwise app will not work and you will need to reinstall it!"
-    echo "Enter your login: "
-    read login
-    echo "Enter your password: "
-    read password
-    echo "Enter your Ngrok authtoken: "
-    read authtoken
-    ./ngrok authtoken $authtoken
-    echo "$login;$password" >> ./CRTClover/server/login.txt
-    echo "Installing succesfully completed!"
-elif [ $install == "n" ]
-then
-    exit 0
-else
-    echo "Invalid value. Please re-run the installer."
-    exit 0
-fi
+echo "RescueClover installation started."
+git clone https://github.com/DevMBS/CRTClover.git
+npm cache clean -f && npm install -g n && npx n install stable
+cd ./CRTClover && npm install --save child_process roslib fs socket.io-client
+mv login.txt ./CRTClover
+echo "Installation successfully completed!"
--- server.js
+++ server.js
@@ -6,127 +6,103 @@ const getTelemetry = new ROSLIB.Service({ ros: ros, name : '/get_telemetry', ser
 const navigate = new ROSLIB.Service({ ros: ros, name : '/navigate', serviceType : 'clover/Navigate' });
 const land = new ROSLIB.Service({ ros: ros, name : '/land', serviceType : 'clover/Land' });
 const navigate_global = new ROSLIB.Service({ ros: ros, name : '/navigate_global', serviceType : 'clover/NavigateGlobal' });
-const request = require("request");
 const fs = require('fs');
-const { MongoClient } = require('mongodb');
-const url = 'https://rescueclover.vercel.app/';
+const {io} = require('socket.io-client')
+const socket = io.connect('https://crtclient.rescueclover.repl.co');
+const url = 'https://crtclient.rescueclover.repl.co/';
 //get login and password of user
-const logindata = fs.readFileSync('login.txt', 'utf8').split(';');
+const uid = fs.readFileSync('login.txt', 'utf8');
+
+
 //connection request
-request.post(
-    url+'connect',
-    { json: { login: logindata[0], password: logindata[1]} },
-    function (error, response, body) {
-        if(response.body.uid == null || response.body.db == null){
-            console.log('Error! Please Re-install the app.');
-        }
-        else{
-            //get uid from server response
-            const uid = String(response.body.uid);
-            const uri = String(response.body.db);
-            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-            //connecting to command database client
-            client.connect(err => {
-                if (err) {
-                    console.log('Connection error: ', err);
-                    throw err;
-                }
-                else{
-                    console.log('Connected to CRTCloversideCommandDB');
-                }
-                //connecting to command db
-                const db = client.db('CRTCloversideCommandDB');
-                const user = db.collection(logindata[0]);
-                function insertOne(collection, data){
-                    try {
-                        collection.insertOne(data);
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-                //starting telemetry stream
-                let telemetrystreaminterval = setInterval(telemetrystream, 1000);
-                //telemetry stream
-                function telemetrystream(){
+socket.on('connect', function(){
+    socket.emit('connectclover', uid);
+    socket.on('connectres'+uid, (user)=> {
+        if(user!= null){
+            console.log('Connected to server');
+            //check emergency and disarm drone
+            getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telem) {
+                let checkEmergencyInterval = setInterval(checkEmergency, 100);
+                let x = telem.vx;
+                let y = telem.vy;
+                let z = telem.vz;
+                function checkEmergency(){
                     getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telemetry) {
-                        if(telemetry.armed!=null && telemetry.z!=null && telemetry.pitch!=null && telemetry.roll!=null && telemetry.yaw!=null && telemetry.cell_voltage!=null){
-                        insertOne(user, {data: 'telemetry', telemetry: {armed: telemetry.armed, z: telemetry.z, lat: telemetry.lat, lon: telemetry.lon, alt: telemetry.alt, pitch: telemetry.pitch, roll: telemetry.roll, yaw: telemetry.yaw, cell_voltage: telemetry.cell_voltage.toFixed(2)}});
-                    }
+                        if(Math.abs(telemetry.vx - x) > 1 || Math.abs(telemetry.vy - y) > 1 || Math.abs(telemetry.vz - z) > 1){
+                            exec("sudo systemctl restart clover", (error, stdout, stderr) => {});
+                        }
                     });
                 }
-                getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telem) {
-                    let checkEmergencyInterval = setInterval(checkEmergency, 100);
-                    let x = telem.vx;
-                    let y = telem.vy;
-                    let z = telem.vz;
-                    function checkEmergency(){
-                        getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telemetry) {
-                            if(Math.abs(telemetry.vx - x) > 1 || Math.abs(telemetry.vy - y) > 1 || Math.abs(telemetry.vz - z) > 1){
-                                exec("sudo systemctl restart clover", (error, stdout, stderr) => {});
-                            }
-                        });
+            });
+
+            let telemetrystreaminterval = setInterval(telemetryStream, 1000);
+            function telemetryStream(){
+                getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telemetry) {
+                    if(telemetry.armed!=null && telemetry.z!=null && telemetry.pitch!=null && telemetry.roll!=null && telemetry.yaw!=null && telemetry.cell_voltage!=null){
+                        socket.emit('telemetry', {uid: uid, armed: telemetry.armed, z: telemetry.z, lat: telemetry.lat, lon: telemetry.lon, alt: telemetry.alt, pitch: telemetry.pitch, roll: telemetry.roll, yaw: telemetry.yaw, cell_voltage: telemetry.cell_voltage.toFixed(2)})
                     }
                 });
-                //handle commands
-                user.watch().on("change", next => {
-                    if(next.operationType == 'insert' && next.fullDocument.data == 'command'){
-                        if(next.fullDocument.command == 'land'){
-                            land.callService(new ROSLIB.ServiceRequest({}), function(result) {
-                            });
+            }
+
+            socket.on('command', (command)=>{
+                if(command.command == 'land'){
+                    land.callService(new ROSLIB.ServiceRequest({}), function(result) {});
+                }
+                else if(command.command == 'hover'){
+                    navigate.callService(new ROSLIB.ServiceRequest({ x: 0.0, y: 0.0, z: 0.0, yaw: 0.0, yaw_rate: 0.0, speed: 0.1, frame_id: 'body' }), function(result) {});
+                }
+                else if(command.command == 'reboot'){
+                    try {
+                        exec("python emergency.py", (error, stdout, stderr) => {});
+                    } 
+                    catch (error) {}
+                    setTimeout(function(){exec("sudo systemctl restart clover", (error, stdout, stderr) => {});}, 200);
+                }
+                else if(command.command == 'photo'){
+                    if (fs.existsSync('./photo.png')){
+                        fs.unlinkSync(__dirname+'/photo.png');
+                    }
+                    exec("python photo.py", (error, stdout, stderr) => {
+                        if(!error){
+                            let pfc = new Buffer(fs.readFileSync(__dirname+'/photo.png')).toString('base64');
+                            socket.emit('photo', {uid: uid, photo: pfc});
+                            fs.unlinkSync(__dirname+'/photo.png');
                         }
-                        else if(next.fullDocument.command == 'hover'){
-                            navigate.callService(new ROSLIB.ServiceRequest({ x: 0.0, y: 0.0, z: 0.0, yaw: 0.0, yaw_rate: 0.0, speed: 0.1, frame_id: 'body' }), function(result) {});
-                        }
-                        else if(next.fullDocument.command == 'reboot'){
-                            try {
-                                exec("python emergency.py", (error, stdout, stderr) => {});
-                            } catch (error) {}
-                            setTimeout(function(){exec("sudo systemctl restart clover", (error, stdout, stderr) => {});}, 200);
-                        }
-                        else if(next.fullDocument.command == 'rth'){
-                            getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telemetry) {
-                                let arming = false;
-                                let data = next.fullDocument.gps;
-                                if(!telemetry.armed){arming = true}
-                                if(data.to == 'user'){
-                                    navigate.callService(new ROSLIB.ServiceRequest({ x: 0.0, y: 0.0, z: data.alt-telemetry, yaw: 0.0, yaw_rate: 0.0, speed: 1, frame_id: 'body', auto_arm: arming }), function(result) {});
-                                    setTimeout(returnTO, (data.alt-telemetry)*1000+8000);
-                                    function returnTO(){
-                                        navigate_global.callService(new ROSLIB.ServiceRequest({ lat: data.lat, lon: data.lon, z: 0.0, yaw: 0.0, yaw_rate: 0.0, speed: data.speed, frame_id: 'body', auto_arm: false }), function(result) {
-                                        });
-                                        let x = data.lat - telemetry.lat;
-                                        let y = data.lon - telemetry.lon;
-                                        setTimeout(action, (Math.sqrt(x*x+y*y)/data.speed)*1000+20000);
-                                        function action(){
-                                            if(data.action == 'hover'){
-                                                navigate.callService(new ROSLIB.ServiceRequest({ x: 0.0, y: 0.0, z: 0.0, yaw: 0.0, yaw_rate: 0.0, speed: 1, frame_id: 'body'}), function(result) {});
-                                            }
-                                            else if(data.action == 'land'){
-                                                land.callService(new ROSLIB.ServiceRequest({}), function(result) {});
-                                            }
-                                        }
+                    });
+                }
+                else if(command.command == 'rth'){
+                    getTelemetry.callService(new ROSLIB.ServiceRequest({ frame_id: '' }), function(telemetry) {
+                        let arming = false;
+                        let data = command.data;
+                        if(!telemetry.armed){arming = true}
+                        if(data.to == 'user'){
+                            navigate.callService(new ROSLIB.ServiceRequest({ x: 0.0, y: 0.0, z: data.alt-telemetry, yaw: 0.0, yaw_rate: 0.0, speed: 1, frame_id: 'body', auto_arm: arming }), function(result) {});
+                            setTimeout(returnTO, (data.alt-telemetry)*1000+8000);
+                            function returnTO(){
+                                navigate_global.callService(new ROSLIB.ServiceRequest({ lat: data.lat, lon: data.lon, z: 0.0, yaw: 0.0, yaw_rate: 0.0, speed: data.speed, frame_id: 'body', auto_arm: false }), function(result) {
+                                });
+                                let x = data.lat - telemetry.lat;
+                                let y = data.lon - telemetry.lon;
+                                setTimeout(action, (Math.sqrt(x*x+y*y)/data.speed)*1000+20000);
+                                function action(){
+                                    if(data.action == 'hover'){
+                                        navigate.callService(new ROSLIB.ServiceRequest({ x: 0.0, y: 0.0, z: 0.0, yaw: 0.0, yaw_rate: 0.0, speed: 1, frame_id: 'body'}), function(result) {});
+                                    }
+                                    else if(data.action == 'land'){
+                                        land.callService(new ROSLIB.ServiceRequest({}), function(result) {});
                                     }
                                 }
-                                else if(next.fullDocument.gps.to == 'takeoff'){
-                                    //TODO
-                                }
-                            });
-                        }
-                        else if(next.fullDocument.command == 'photo'){
-                            if (fs.existsSync('./photo.png')){
-                                fs.unlinkSync(__dirname+'/photo.png');
                             }
-                            exec("python photo.py", (error, stdout, stderr) => {
-                                if(!err){
-                                    user.deleteMany({command: 'photo'});
-                                    let pfc = new Buffer(fs.readFileSync(__dirname+'/photo.png')).toString('base64');
-                                    insertOne(user, {data: 'photo', photo: pfc});
-                                    fs.unlinkSync(__dirname+'/photo.png');
-                                }
-                            });
                         }
-                    }
-                });
+                        else if(data.to == 'takeoff'){
+                            //TODO
+                        }
+                    });
+                }
             });
         }
+        else{
+            console.log('Authentification failed. Please re-install the app.')
+        }
+    });
 });
