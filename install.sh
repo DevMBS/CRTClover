d49da01fae681eb828545bf7664032c4dbb7f7a8
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-02 16:05:53 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#Clover Rescue Team's official Cloverside software installer
-
-echo "RescueClover installation started."
-git clone https://github.com/DevMBS/CRTClover.git
-npm cache clean -f && npm install -g n && npx n install stable
-cd ./CRTClover && npm install --save child_process roslib fs socket.io-client
-mv login.txt ./CRTClover
-echo "Installation successfully completed!"
