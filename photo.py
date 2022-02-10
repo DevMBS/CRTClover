7be801dfd9b4f03848bbd82c2d4f82bdd08a0faf
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2022-02-10 23:22:35 +0300

    moved to mavros.py

--- photo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import cv2
-import rospy
-rospy.init_node('computer_vision_sample')
-from cv_bridge import CvBridge
-from sensor_msgs.msg import Image
-bridge = CvBridge()
-img = bridge.imgmsg_to_cv2(rospy.wait_for_message('main_camera/image_raw', Image), 'bgr8')
-cv2.imwrite('/var/www/CRTClover/photo.png', img)
